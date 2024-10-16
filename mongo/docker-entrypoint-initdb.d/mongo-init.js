88f06183d629dc1b6073f455236736fc4f3ec112
Iago Fagundes <iagof.elesbao@gmail.com>
2024-10-16 14:23:50 -0300

    refactor: update containers

--- mongo/docker-entrypoint-initdb.d/mongo-init.js
+++ mongo/docker-entrypoint-initdb.d/mongo-init.js
@@ -4,6 +4,16 @@ console.log('COLLECTION: ' + process.env.MONGO_INITDB_COLLECTION)
 console.log('USER: ' + process.env.MONGO_INITDB_ROOT_USERNAME)
 console.log('PASSWORD: ' + process.env.MONGO_INITDB_ROOT_PASSWORD)
 console.log('###################################')
+//
+// db = db.getSiblingDB('admin');
+//
+// db.createUser(
+//     {
+//         user: process.env.MONGO_INITDB_ROOT_USERNAME,
+//         pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
+//         roles: [ { role: 'root', db: 'admin' } ]
+//     },
+// );
 
 db = db.getSiblingDB(process.env.MONGO_INITDB_DATABASE);
 
@@ -11,8 +21,10 @@ db.createUser(
   {
     user: process.env.MONGO_INITDB_ROOT_USERNAME,
     pwd: process.env.MONGO_INITDB_ROOT_PASSWORD,
-    roles: [{ role: 'readWrite', db: process.env.MONGO_INITDB_DATABASE }],
+    roles: [{ role: 'root', db: process.env.MONGO_INITDB_DATABASE }],
   },
 );
+
+db.grantRolesToUser(process.env.MONGO_INITDB_ROOT_USERNAME, ["readWrite"])
 
 db.createCollection(process.env.MONGO_INITDB_COLLECTION);
